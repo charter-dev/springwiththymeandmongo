755a621afddab42c782d59f5022aca569fdc6f41
charter <charter43@gmail.com>
2022-06-14 11:05:17 +0700

    commit
    commit

---
+++ mongodbapps/src/main/java/com/springboot/mongo/mongodbapps/MongodbappsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.mongo.mongodbapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbappsApplication.class, args);
+	}
+
+}
---
+++ mongodbapps/src/test/java/com/springboot/mongo/mongodbapps/MongodbappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mongo.mongodbapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
